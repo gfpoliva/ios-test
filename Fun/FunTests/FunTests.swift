63115c7b183f6248d1ddc9960e31b5c0f5c070f1
Gabriel Oliva <gfpoliva@gmail.com>
2021-10-15 17:54:19 -0300

    projeto

---
+++ Fun/FunTests/FunTests.swift
@@ -0,0 +1,21 @@
+//
+//  FunTests.swift
+//  FunTests
+//
+//  Created by Gabriel Oliva on 15/10/21.
+//
+
+import XCTest
+@testable import Fun
+
+class FunTests: XCTestCase {
+
+    func testShouldPass() throws {
+        XCTAssertTrue(true);
+    }
+    
+    func testShouldFail() throws {
+        XCTAssertTrue(false);
+    }
+
+}
---
+++ Fun/FunUITests/FunUITests.swift
@@ -0,0 +1,12 @@
+//
+//  FunUITests.swift
+//  FunUITests
+//
+//  Created by Gabriel Oliva on 15/10/21.
+//
+
+import XCTest
+
+class FunUITests: XCTestCase {
+
+}
